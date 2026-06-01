7a705b04aa9a31235c0cc20e0d03b8a751544eb8
angelical3452 <mo909373@gmail.com>
2026-06-01 15:12:45 -0500

    fix double click register

--- src/Components/Boton/Boton.jsx
+++ src/Components/Boton/Boton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "@mui/material/Button";
 
-const Boton = ({ value, funcion, className, disabled }) => {
+const Boton = ({ value, funcion, className, disabled = true }) => {
   return (
     <button className={className} onClick={funcion} disabled={disabled}>
       {value}
